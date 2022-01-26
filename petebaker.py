ec5bcd7bbf7830c07563a2eea54b3eff8b5680a0
Tiago da Silva <tiagohs96@gmail.com>
2022-01-26 20:14:25 +0000

    Just putting out a solution to see if my solution will work in a happy scenario. Will try to make it more pythonic and add some stress test

--- petebaker.py
+++ petebaker.py
@@ -4,3 +4,17 @@ def cakes(recipe, available):
 
     Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
     """
+
+    s = {int(available[k]/recipe[k]) if k in available.keys() else 0 for k in recipe.keys()}
+
+    return min(s)
+
+if __name__ == '__main__':
+    #Not a proper unit test, just checking if the general logic works
+    recipe = {"flour": 500, "sugar": 200, "eggs": 1}
+    available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+    print(cakes(recipe, available) == 2)
+
+    recipe = {"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}
+    available = {"sugar": 500, "flour": 2000, "milk": 2000}
+    print(cakes(recipe, available) == 0)
